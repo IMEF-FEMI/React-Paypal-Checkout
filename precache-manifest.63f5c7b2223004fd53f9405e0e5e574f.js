420ea66e9403687eb6311421f1378fb484057b23
IMEF <bolajifemi28@gmail.com>
2019-07-26 13:18:16 +0100

    Updates

---
+++ precache-manifest.63f5c7b2223004fd53f9405e0e5e574f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d2768365d4b796fb593b86f0c1911e",
+    "url": "/React-Paypal-Checkout/index.html"
+  },
+  {
+    "revision": "e9a26b7c70298caf885b",
+    "url": "/React-Paypal-Checkout/static/css/main.ab915e9f.chunk.css"
+  },
+  {
+    "revision": "86ce2f91c57bd0c2a46a",
+    "url": "/React-Paypal-Checkout/static/js/2.083dd22f.chunk.js"
+  },
+  {
+    "revision": "e9a26b7c70298caf885b",
+    "url": "/React-Paypal-Checkout/static/js/main.1eadb11c.chunk.js"
+  },
+  {
+    "revision": "3197f15d2042ea69f430",
+    "url": "/React-Paypal-Checkout/static/js/runtime~main.affd2a80.js"
+  },
+  {
+    "revision": "5f8453429f6c6b0c9dcf1f0be1e70cc9",
+    "url": "/React-Paypal-Checkout/static/media/car.5f845342.jpg"
+  }
+]);
